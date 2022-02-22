ba632ad093a75f1de5246b10578cc35759b33125
Joshua Oti <93266206+joshoti@users.noreply.github.com>
2022-02-22 18:53:57 +0100

    Add files via upload

--- oti/files/mainwindow.py
+++ oti/files/mainwindow.py
@@ -14,6 +14,7 @@ class Backend(QObject):
         self.adminlog = ""
         self.student = ""
         self.studentlog = ""
+        self.availbal = 6000
         self.activity = ""
         self.recipient = ""
         self.recipientlog = ""
@@ -21,9 +22,9 @@ class Backend(QObject):
 
     report = []
     #supersheet = {'100': '0000'}
-    supersheet = {'1': ''}
+    supersheet = {'': ''}
     #adminsheet = {'200': '0000'}
-    adminsheet = {'2': '0000'}
+    adminsheet = {'': '0000'}
     #customersheet = {'': '0000'}
     customersheet = {'3': '0000'}
     pageindex = {
@@ -82,25 +83,40 @@ class Backend(QObject):
         self.admin = ""
         self.adminlog = ""
 
+    # function will be called to extract important info
+    '''def userdetails(self, information):
+        data = gspread.open('customer.doc')[information]
+        return data'''
+        
     loggeduser = Signal(str)
+    accbalance = Signal(float)
     
     @Slot(list)
     def studentuser(self, user):
+        #self.student, password, balance, = userdetails(self, user)
         print(f"Hey student, {user[0]}")
         self.student = user[0]
         u_password = user[1]
         self.studentlog = user[2]
         if self.student in self.customersheet:
             password = self.customersheet[self.student]
-            if u_password == password: self.loaded(self.pageindex[self.activity]); self.loggeduser.emit(self.student)
+            if u_password == password: self.loaded(self.pageindex[self.activity]); self.switchfeature()
             else: self.loaded('close'); self.incorrect.emit(1)
         else: self.loaded('close'); self.incorrect.emit(1)
 
+    featuremode = Signal(str)
+    
     @Slot(str)
     def feature(self, activity):
         print(f"{activity} chosen")
         self.activity = activity
 
+    @Slot()
+    def switchfeature(self):
+        self.loggeduser.emit(self.student)
+        self.featuremode.emit(self.activity)
+        self.accbalance.emit(self.availbal)
+        
     @Slot(float)
     def purchasefeature(self, amount):
         print(f"{amount} involved")
@@ -115,17 +131,55 @@ class Backend(QObject):
         self.recipientlog = details[2]
         if self.recipient not in self.customersheet: self.loaded('close')
 
+    invalid = Signal(int)
+    @Slot(list)
+    def registeruser(self, details):
+        print(f"{details[0]} has Registered been")
+        self.student = details[0]
+        #check if user exists in database
+        #if self.student in gs('students'): self.invalid.emit(1); self.log(4) # failed
+        """if details not in gs('students'):
+            with open (gs('students'), 'a+') as file:
+                file.write(details)
+                self.log(5)
+        else: self.log(4)"""
+        self.log(5)
+
+    @Slot()
+    def userlogout(self):
+        print(f"Bye user, {self.student} at {datetime.datetime.now().__str__()[:19]}")
+        self.student = ""
+        self.studentlog = ""
+        self.activity = ""
 
     @Slot(int)
     def log(self, instruction):
         # Purchase Success
-        if instruction == 1: print(f"Success: {self.student}'s {self.activity} {self.amount:.2f} by {datetime.datetime.now().__str__()[:19]}"); self.report.append(f"{self.activity} Success {self.student} {self.amount:.2f} {datetime.datetime.now().__str__()[:19]}")
+        if instruction == 1:
+            message = f"Success: {self.student} {self.activity} {self.amount:.2f} by {datetime.datetime.now().__str__()[:19]}"
+            print(message) ; self.report.append(message)
         # Purchase failed
-        elif instruction == 0: print(f"Failed: {self.student}'s {self.activity} {self.amount:.2f} by {datetime.datetime.now().__str__()[:19]}"); self.report.append(f"{self.activity} Failed {self.student} {self.amount:.2f} {datetime.datetime.now().__str__()[:19]}")
+        elif instruction == 0:
+            message = f"Failed: {self.student} {self.activity} {self.amount:.2f} by {datetime.datetime.now().__str__()[:19]}"
+            print(message) ; self.report.append(message)
+
         # Transfer Success
-        elif instruction == 3: print(f"Success: {self.student}'s {self.activity} {self.amount:.2f} to {self.recipient} by {datetime.datetime.now().__str__()[:19]}"); self.report.append(f"{self.activity} Success {self.student} to {self.recipient} {self.amount:.2f} {datetime.datetime.now().__str__()[:19]}")
+        elif instruction == 3:
+            message = f"Success: {self.student} {self.activity} {self.amount:.2f} to {self.recipient} by {datetime.datetime.now().__str__()[:19]}"
+            print(message) ; self.report.append(message)
         # Transfer failed
-        elif instruction == 2: print(f"Failed: {self.student}'s {self.activity} {self.amount:.2f} to {self.recipient} by {datetime.datetime.now().__str__()[:19]}"); self.report.append(f"{self.activity} Failed {self.student} to {self.recipient} {self.amount:.2f} {datetime.datetime.now().__str__()[:19]}")
+        elif instruction == 2:
+            message = f"Failed: {self.student} {self.activity} {self.amount:.2f} to {self.recipient} by {datetime.datetime.now().__str__()[:19]}"
+            print(message) ; self.report.append(message)
+            
+        # Registration success
+        elif instruction == 5:
+            message = f"Success: {self.student} {self.activity} by {datetime.datetime.now().__str__()[:19]}"
+            print(message) ; self.report.append(message)
+        # Registration failed
+        elif instruction == 4:
+            message = f"Failed: {self.student} {self.activity} by {datetime.datetime.now().__str__()[:19]}"
+            print(message) ; self.report.append(message)
         #Sends email to user
         def writeout(reportfile):
             # read report and rewrite or append value
